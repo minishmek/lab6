c690cb3c238547c46640815d7e980481b1095a63
minishmek <minishmek@gmail.com>
2021-05-19 01:13:35 +0300

    my first commit

---
+++ src/main/java/com/example/informationsystem/InformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.informationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationSystemApplication.class, args);
+    }
+
+}
